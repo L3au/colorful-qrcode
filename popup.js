98cd501b0ac4b0b0368711afdd1c1807f1bcb218
L3au <leshu.lau@gmail.com>
2015-07-03 22:46:47 +0800

    optimized

--- popup.js
+++ popup.js
@@ -1,18 +1,20 @@
-chrome.tabs.query({
-  active       : true,
-  currentWindow: true
-}, function (tabs) {
-  var url = tabs[0].url;
-  var qr  = document.getElementById("qr");
+requestAnimationFrame(function () {
+  chrome.tabs.query({
+    active       : true,
+    currentWindow: true
+  }, function (tabs) {
+    var url = tabs[0].url;
+    var qr  = document.getElementById("qr");
 
-  new QRCode(qr, {
-    text        : url,
-    width       : 240,
-    height      : 240,
-    colorDark   : randomColor({
-      luminosity: 'bright'
-    }),
-    colorLight  : "#ffffff",
-    correctLevel: QRCode.CorrectLevel.L
+    new QRCode(qr, {
+      text        : url,
+      width       : 240,
+      height      : 240,
+      colorDark   : randomColor({
+        luminosity: 'bright'
+      }),
+      colorLight  : "#ffffff",
+      correctLevel: QRCode.CorrectLevel.L
+    });
   });
 });
