f1d2bcc3b64d82f4db1c42a4f129a6a2d7fdfcc4
L3au <leshu.lau@gmail.com>
2015-07-03 16:53:07 +0800

    fix

--- popup.js
+++ popup.js
@@ -1,42 +1,9 @@
-function getLocalIPs(callback) {
-  var ips = [];
-
-  var RTCPeerConnection = window.RTCPeerConnection ||
-      window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
-
-  var pc = new RTCPeerConnection({
-    iceServers: []
-  });
-
-  pc.createDataChannel('');
-
-  pc.onicecandidate = function (e) {
-    if (!e.candidate) {
-      callback(ips);
-      return;
-    }
-
-    var ip = /^candidate:.+ (\S+) \d+ typ/.exec(e.candidate.candidate)[1];
-    if (ips.indexOf(ip) == -1) {
-      ips.push(ip);
-    }
-  };
-  pc.createOffer(function (sdp) {
-    pc.setLocalDescription(sdp);
-  }, function onerror() {
-  });
-}
-
 chrome.tabs.getSelected(null, function (tab) {
-  // http://qr.liantu.com/api.php?fg=ff6600&el=l&w=233&m=0&
-  // https://chart.googleapis.com/chart?cht=qr&chs=233x233&choe=UTF-8&chld=L|0&chl=
-
   var url    = new URL(tab.url);
   var qr     = document.getElementById("qr");
   var qrcode = new QRCode(qr, {
     width       : 240,
     height      : 240,
-    //colorDark : "#008B8B",
     colorDark   : randomColor({
       luminosity: 'bright'
     }),
@@ -44,30 +11,5 @@ chrome.tabs.getSelected(null, function (tab) {
     correctLevel: QRCode.CorrectLevel.M
   });
 
-  function showQr(url) {
-    //var href = 'https://chart.googleapis.com/chart?cht=qr&chs=233x233&choe=UTF-8&chld=L|0&chl=';
-    //href += encodeURIComponent(url) + '&t=' + Math.random();
-
-    qrcode.makeCode(url);
-  }
-
-  switch (url.host) {
-    case 'sync.wacai.com':
-      getLocalIPs(function (ips) {
-        url.host = ips[0] + ':3000';
-
-        showQr(url.href);
-      });
-      break;
-    case 'dev.wacai.com':
-      getLocalIPs(function (ips) {
-        url.host = ips[0] + ':8080';
-
-        showQr(url.href);
-      });
-      break;
-    default :
-      showQr(tab.url);
-      break;
-  }
+  qrcode.makeCode(url);
 });
