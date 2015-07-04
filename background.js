3be1bd64de8bd9518c1ec1904012c217afa978ea
L3au <leshu.lau@gmail.com>
2015-07-04 22:33:00 +0800

    add custom generator

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
 // 一个好奇怪的问题。。
 // 在windows chrome下如果manifest不设置background，browser action时鼠标指针总会转菊花，popup明显有延迟
 // 所以就这样喽。。如果你知道原因记得告诉我哦！
+
+chrome.runtime.onInstalled.addListener(function (details) {
+  if (details.reason == 'install') {
+    var url = 'chrome-extension://' + chrome.runtime.id + '/readme.html';
+
+    chrome.tabs.create({
+      url   : url,
+      active: true
+    });
+  }
+});
--- popup.js
+++ popup.js
@@ -1,69 +1,51 @@
-function getLocalIPs(callback) {
-  var ips = [];
+chrome.tabs.query({
+  active: true,
+  currentWindow: true
+}, function(tabs) {
+  var url = tabs[0].url;
+  var qr = document.getElementById("qr");
+  var txt = qr.querySelector('textarea');
+  var img;
 
-  var RTCPeerConnection = window.RTCPeerConnection ||
-      window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
-
-  var pc = new RTCPeerConnection({
-    iceServers: []
+  var qrcode = new QRCode(qr, {
+    text: url,
+    width: 240,
+    height: 240,
+    colorDark: randomColor({
+      luminosity: 'bright'
+    }),
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.L
   });
 
-  pc.createDataChannel('');
+  img = qr.querySelector('img');
+  img.addEventListener('click', function () {
+    img.className = 'hide';
 
-  pc.onicecandidate = function (e) {
-    if (!e.candidate) {
-      callback(ips);
+    txt.style.display = 'block';
+    txt.select();
+  });
+
+  document.addEventListener('keypress', function (e) {
+    if (e.which !== 13) {
       return;
     }
 
-    var ip = /^candidate:.+ (\S+) \d+ typ/.exec(e.candidate.candidate)[1];
-    if (ips.indexOf(ip) == -1) {
-      ips.push(ip);
+    e.preventDefault();
+
+    if (img.className == 'hide') {
+      txt.style.display = 'none';
+
+      var val = txt.value.trim();
+
+      qrcode.makeCode(val ? val : url);
+
+      img.className = '';
+    } else {
+      img.className = 'hide';
+
+      txt.style.display = 'block';
+      txt.select();
     }
-  };
-  pc.createOffer(function (sdp) {
-    pc.setLocalDescription(sdp);
-  }, function onerror() {
   });
-}
-
-
-chrome.tabs.query({
-  active       : true,
-  currentWindow: true
-}, function (tabs) {
-  var url = new URL(tabs[0].url);
-  var qr  = document.getElementById("qr");
-
-  function showQr(url) {
-    new QRCode(qr, {
-      text        : url,
-      width       : 240,
-      height      : 240,
-      colorDark   : randomColor({
-        luminosity: 'bright'
-      }),
-      colorLight  : "#ffffff",
-      correctLevel: QRCode.CorrectLevel.L
-    });
-  }
-
-  switch (url.host) {
-    case 'sync.wacai.com':
-      getLocalIPs(function (ips) {
-        url.host = ips[0] + ':3000';
-
-        showQr(url.href);
-      });
-      break;
-    case 'dev.wacai.com':
-      getLocalIPs(function (ips) {
-        url.host = ips[0] + ':8080';
-
-        showQr(url.href);
-      });
-      break;
-    default :
-      showQr(url.href);
-  }
 });
