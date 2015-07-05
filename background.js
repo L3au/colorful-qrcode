9f96a1672e7c40ccee82d94ddbfa2ba4e1d7895f
L3au <leshu.lau@gmail.com>
2015-07-05 12:19:35 +0800

    update

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 // 一个好奇怪的问题。。
-// 在windows chrome下如果manifest不设置background，browser action时鼠标指针总会转菊花，popup明显有延迟
-// 所以就这样喽。。如果你知道原因记得告诉我哦！
+// 在windows chrome下如果manifest不设置background，browser action时鼠标指针总会转菊花，popup有明显延迟
+// 如果你知道原因记得告诉我哦！
 
 chrome.runtime.onInstalled.addListener(function (details) {
   if (details.reason == 'install') {
