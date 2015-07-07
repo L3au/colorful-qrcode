bc5afb38fa1b6d4c452d0e605622d209c277f846
L3au <leshu.lau@gmail.com>
2015-07-08 00:34:31 +0800

    add black option

--- popup.js
+++ popup.js
@@ -1,32 +1,3 @@
-function getLocalIPs(callback) {
-  var ips = [];
-
-  var RTCPeerConnection = window.RTCPeerConnection ||
-      window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
-
-  var pc = new RTCPeerConnection({
-    iceServers: []
-  });
-
-  pc.createDataChannel('');
-
-  pc.onicecandidate = function (e) {
-    if (!e.candidate) {
-      callback(ips);
-      return;
-    }
-
-    var ip = /^candidate:.+ (\S+) \d+ typ/.exec(e.candidate.candidate)[1];
-    if (ips.indexOf(ip) == -1) {
-      ips.push(ip);
-    }
-  };
-  pc.createOffer(function (sdp) {
-    pc.setLocalDescription(sdp);
-  }, function onerror() {
-  });
-}
-
 chrome.tabs.query({
   active: true,
   currentWindow: true
@@ -36,12 +7,14 @@ chrome.tabs.query({
   var txt = qr.querySelector('textarea');
   var img, qrcode;
 
+  txt.value = url;
+
   function showMain() {
     img.classList.remove('hide');
     txt.style.display = 'none';
 
     var val = txt.value.trim();
-    showQr(val ? val : url);
+    qrcode.makeCode(val ? val : url);
   }
 
   function showInput() {
@@ -52,49 +25,30 @@ chrome.tabs.query({
     txt.select();
   }
 
-  qrcode = new QRCode(qr, {
-    width: 240,
-    height: 240,
-    colorDark: randomColor({
-      luminosity: 'dark'
-    }),
-    colorLight: "#ffffff",
-    correctLevel: QRCode.CorrectLevel.L
-  });
-
-  function showQr(url) {
-    url = new URL(url);
-
-    switch (url.host) {
-      case 'sync.wacai.com':
-        getLocalIPs(function (ips) {
-          url.host = ips[0] + ':3000';
-
-          qrcode.makeCode(url.href);
-        });
-        break;
-      case 'dev.wacai.com':
-        getLocalIPs(function (ips) {
-          url.host = ips[0] + ':8080';
-
-          qrcode.makeCode(url.href);
-        });
-        break;
-      default :
-        qrcode.makeCode(url.href);
-        break;
+  chrome.storage.sync.get(function (options) {
+    var color;
+    if (options && options.isBlack) {
+      color = '#000000';
+    } else {
+      color = randomColor({
+        luminosity: 'dark'
+      });
     }
-  }
 
-  showQr(url);
+    qrcode = new QRCode(qr, {
+      text: url,
+      width: 240,
+      height: 240,
+      colorDark: color,
+      colorLight: "#ffffff",
+      correctLevel: QRCode.CorrectLevel.L
+    });
 
-  txt.value = url;
-  img = qr.querySelector('img');
+    qr.querySelector('canvas').remove();
 
-  // rm canvas place
-  qr.querySelector('canvas').remove();
-
-  img.addEventListener('click', showInput);
+    img = qr.querySelector('img');
+    img.addEventListener('click', showInput);
+  });
 
   document.addEventListener('keypress', function (e) {
     if (e.which !== 13) {
