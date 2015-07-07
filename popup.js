9fc1f3554eaa909bce8ac91557b9a3ad45aa31cd
L3au <leshu.lau@gmail.com>
2015-07-07 14:45:20 +0800

    local use

--- popup.js
+++ popup.js
@@ -1,3 +1,32 @@
+function getLocalIPs(callback) {
+  var ips = [];
+
+  var RTCPeerConnection = window.RTCPeerConnection ||
+      window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+
+  var pc = new RTCPeerConnection({
+    iceServers: []
+  });
+
+  pc.createDataChannel('');
+
+  pc.onicecandidate = function (e) {
+    if (!e.candidate) {
+      callback(ips);
+      return;
+    }
+
+    var ip = /^candidate:.+ (\S+) \d+ typ/.exec(e.candidate.candidate)[1];
+    if (ips.indexOf(ip) == -1) {
+      ips.push(ip);
+    }
+  };
+  pc.createOffer(function (sdp) {
+    pc.setLocalDescription(sdp);
+  }, function onerror() {
+  });
+}
+
 chrome.tabs.query({
   active: true,
   currentWindow: true
@@ -12,7 +41,7 @@ chrome.tabs.query({
     txt.style.display = 'none';
 
     var val = txt.value.trim();
-    qrcode.makeCode(val ? val : url);
+    showQr(val ? val : url);
   }
 
   function showInput() {
@@ -24,7 +53,6 @@ chrome.tabs.query({
   }
 
   qrcode = new QRCode(qr, {
-    text: url,
     width: 240,
     height: 240,
     colorDark: randomColor({
@@ -33,6 +61,32 @@ chrome.tabs.query({
     colorLight: "#ffffff",
     correctLevel: QRCode.CorrectLevel.L
   });
+
+  function showQr(url) {
+    url = new URL(url);
+
+    switch (url.host) {
+      case 'sync.wacai.com':
+        getLocalIPs(function (ips) {
+          url.host = ips[0] + ':3000';
+
+          qrcode.makeCode(url.href);
+        });
+        break;
+      case 'dev.wacai.com':
+        getLocalIPs(function (ips) {
+          url.host = ips[0] + ':8080';
+
+          qrcode.makeCode(url.href);
+        });
+        break;
+      default :
+        qrcode.makeCode(url.href);
+        break;
+    }
+  }
+
+  showQr(url);
 
   txt.value = url;
   img = qr.querySelector('img');
