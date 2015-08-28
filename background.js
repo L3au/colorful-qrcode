88686cebb29d45cd8e70739e63cdb6b06e4da2ea
L3au <leshu.lau@gmail.com>
2015-08-28 22:56:37 +0800

    add update tip

--- background.js
+++ background.js
@@ -1,14 +1,13 @@
 // 一个好奇怪的问题。。
 // 在windows chrome下如果manifest不设置background，browser action时鼠标指针总会转菊花，popup有明显延迟
-// 如果你知道原因记得告诉我哦！
 
 chrome.runtime.onInstalled.addListener(function (details) {
-  if (details.reason == 'install') {
-    var url = 'chrome-extension://' + chrome.runtime.id + '/readme.html';
+    if (details.reason == 'install' || details.reason == 'update') {
+        var url = 'chrome-extension://' + chrome.runtime.id + '/readme.html';
 
-    chrome.tabs.create({
-      url   : url,
-      active: true
-    });
-  }
+        chrome.tabs.create({
+            url   : url,
+            active: true
+        });
+    }
 });
